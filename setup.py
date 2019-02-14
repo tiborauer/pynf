90e80e72f62c011e013ac2f0e6fd01ed7602d485
tiborauer <tibor.auer@gmail.com>
2019-02-14 17:13:37 +0000

    DEBUG - pip install

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='tibor.auer@gmail.com',
     
     packages=['pynf', 'pynf.flappybird'],
-    include_package_data=True
+    include_package_data=True,
     install_requires=['numpy','scipy','matplotlib','pyniexp@git+https://github.com/tiborauer/pyniexp.git'],
 	
     version='0.3.2',
