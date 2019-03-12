948d3b5b462ca184a40466eb352b1b4634aa23c5
MRI <tibor.auer@gmail.com>
2019-03-12 18:04:03 +0000

    U{DATE: example dicomexport

--- example_dicomexport.py
+++ example_dicomexport.py
@@ -3,10 +3,10 @@ from pynf.dicomexport import TcpDicom
 rtd = TcpDicom()
 data = {
     'watch':r'C:\RT\rt',
-    'LastName':'TEST_SUBJECT',
-    'ID': 'RH'
+    'LastName':'Test_Subject',
+    'ID': 'RHUL'
 }
 rtd.set_header_from_Dicom(data)
-rtd.open_as_server()
-rtd.receive_initial()
-dat = rtd.receive_scan()
+rtd.open_as_server(); rtd.receive_initial()
+dat = []
+for n in range(0,10): dat.append(rtd.receive_scan())
